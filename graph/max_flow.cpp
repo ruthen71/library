e113907b54a88bed31c01d173cff5175df0c2236
ruthen71 <takahero2016@gmail.com>
2021-12-04 18:10:05 +0900

    最大流(Ford_Fulkerson)をstructに

--- graph/max_flow.cpp
+++ graph/max_flow.cpp
@@ -1,68 +1,55 @@
-// max flow
-// verified: https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
+// max flow
+// verified: https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A
+// https://onlinejudge.u-aizu.ac.jp/status/users/ruthen71/submissions/1/GRL_6_A/judge/6093346/C++14
+// https://atcoder.jp/contests/abc205/submissions/27687929
 
-#define rep(i, n) for (ll i = 0; i < (n); i++)
-#define sz(x) ((int)(x).size())
-#define pb push_back
-const ll INF = 1ll << 60;
+// Cap: int / long long
+template <class Cap>
+struct ford_fulkerson {
+    struct edge {
+        int to;
+        Cap cap;
+        int rev;
+    };
 
-struct edge {
-    int to;
-    ll cap;
-    int rev;
-};
+    int V;
+    vector<vector<edge>> G;
+    int timestamp;
+    vector<int> used;
+    ford_fulkerson(int V) : V(V), G(V), timestamp(0), used(V, -1) {}
 
-void add_edge(int from, int to, ll cap, vector<vector<edge>> &g) {
-    g[from].push_back((edge){to, cap, g[to].size()});
-    g[to].push_back((edge){from, 0, g[from].size()-1});
-    return;
-}
+    void add_edge(int from, int to, Cap cap) {
+        G[from].push_back((edge){to, cap, (int)G[to].size()});
+        G[to].push_back((edge){from, 0, (int)G[from].size() - 1});
+    }
 
-ll dfs(int v, int t, ll f, vector<bool> &used, vector<vector<edge>> &g) {
-    if (v == t) return f;
-    used[v] = true;
-    for (auto &e : g[v]) {
-        if (!used[e.to] && e.cap > 0) {
-            ll d = dfs(e.to, t, min(f, e.cap), used, g);
-            if (d > 0) {
-                e.cap -= d;
-                g[e.to][e.rev].cap += d;
-                return d;
-            }
+    Cap max_flow(int s, int t) { return max_flow(s, t, numeric_limits<Cap>::max()); }
+    Cap max_flow(int s, int t, Cap flow_limit) {
+        Cap flow = 0;
+        while (1) {
+            Cap f = dfs(s, t, flow_limit);
+            if (f == 0) return flow;
+            flow += f;
+            timestamp++;
         }
     }
-    return 0;
-}
 
-ll max_flow(int s, int t, vector<vector<edge>> g) {
-    // gを参照渡しにしたいときには&gにする
-    ll flow = 0, n = sz(g);
-    while (1) {
-        vector<bool> used(n, 0);
-        ll f = dfs(s, t, INF, used, g);
-        if (f == 0) return flow;
-        flow += f;
+    Cap dfs(int v, int t, Cap f) {
+        if (v == t) return f;
+        used[v] = timestamp;  // seen
+        for (auto &e : G[v]) {
+            if (e.cap > 0 && used[e.to] != timestamp) {
+                Cap d = dfs(e.to, t, min(f, e.cap));
+                if (d > 0) {
+                    e.cap -= d;
+                    G[e.to][e.rev].cap += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
     }
-}
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<edge>> g(n);
-    rep(i, m) {
-        int v, u, c;
-        cin >> v >> u >> c;
-        // v -> u に コスト c
-        g[v].pb({u, c, sz(g[u])});
-        g[u].pb({v, 0, sz(g[v]) - 1});
-    }
-    ll ans = max_flow(0, n - 1, g);
-    cout << ans << endl;
-    return 0;
-}
+};
