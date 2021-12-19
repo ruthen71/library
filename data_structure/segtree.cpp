ae4fe6700c0cb42f08def9b8407dd3966e1686fa
ruthen71 <takahero2016@gmail.com>
2021-12-19 18:54:23 +0900

    セグ木を抽象化

--- data_structure/segtree.cpp
+++ data_structure/segtree.cpp
@@ -3,28 +3,31 @@ using namespace std;
 
 template <class T>
 struct segtree {
-    int _n;
+    // ↓↓↓↓↓ change here! ↓↓↓↓↓
+    T f(T a, T b) { return min(a, b); }
+    T e() { return INT_MAX; }
+    // ↑↑↑↑↑ change here! ↑↑↑↑↑
+    int n, _n;
     vector<T> dat;
-    segtree() {}
-    segtree(int n) {
+    segtree(int n) : n(n) {
         _n = 1;
         while (_n < n) _n *= 2;
-        dat = vector<T>(2 * _n - 1, 0);
+        dat = vector<T>(2 * _n - 1, e());
     }
     void update(int k, T a) {
         k += _n - 1;
         dat[k] = a;
         while (k > 0) {
             k = (k - 1) / 2;
-            dat[k] = max(dat[2 * k + 1], dat[2 * k + 2]);
+            dat[k] = f(dat[2 * k + 1], dat[2 * k + 2]);
         }
     }
-    T query(int a, int b, int k = 0, int l = 0, int r = -1) {
-        if (r == -1) r = _n;
-        if (r <= a || b <= l) return 0;
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) return e();
         if (a <= l && r <= b) return dat[k];
         T vl = query(a, b, 2 * k + 1, l, (l + r) / 2);
         T vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
-        return max(vl, vr);
+        return f(vl, vr);
     }
+    T query(int a, int b) { return query(a, b, 0, 0, _n); }
 };
