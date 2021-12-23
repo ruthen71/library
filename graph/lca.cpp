e59d2a457de54581ed58c0bb3e6a77d0b1bac12a
ruthen71 <takahero2016@gmail.com>
2021-12-23 23:07:42 +0900

    LCA(最近共通祖先)をを構造体に変更

--- graph/lca.cpp
+++ graph/lca.cpp
@@ -2,77 +2,87 @@
 // verified
 // AOJ GRL_5_C LCA: Lowest Common Ancestor
 // https://onlinejudge.u-aizu.ac.jp/status/users/ruthen71/submissions/1/GRL_5_C/judge/5199828/C++14
-
+// ↓new!↓
+// https://onlinejudge.u-aizu.ac.jp/status/users/ruthen71/submissions/1/GRL_5_C/judge/6150132/C++14
 
 #include <bits/stdc++.h>
 using namespace std;
 
-
-using ll = long long;
-#define rep(i,n) for (ll i = 0; i < (n); i++)
-#define pb push_back
-
-const int N = 100005;
-ll depth[N], parent[N];
-ll dbl[35][N];
-vector<ll> g[N];
-// depthとparentを計算
-void dfs(ll par, ll cur) {
-    if (par == -1) depth[cur] = 0;
-    else depth[cur] = depth[par]+1;
-    parent[cur] = par;
-    for (ll nx : g[cur]) {
-        if (nx == par) continue;
-        dfs(cur, nx);
+struct lowest_common_ancestor {
+    vector<vector<int>> G;
+    vector<int> depth;
+    vector<vector<int>> parent;
+    int n, LOG;  // n = 頂点数, root = 根とする頂点番号, LOG = ダブリングする深さ
+    lowest_common_ancestor(int n) : n(n), LOG(32 - __builtin_clz(n)), G(n) {}
+    void add_edge(int u, int v) {
+        // 無向辺{u, v}を加える
+        G[u].push_back(v);
+        G[v].push_back(u);
     }
-}
-// LCAを計算
-ll lca(ll u, ll v) {
-    if (depth[u] > depth[v]) swap(u, v);    // uの方が浅い
-    rep(i,30) {
-        if ((depth[v]-depth[u])>>i&1) v = dbl[i][v];
-    }
-    if (u == v) return u;
-    // 同じ深さになった
-    for (int i = 29; i >= 0; i--) {
-        if (dbl[i][u] != dbl[i][v]) {
-            u = dbl[i][u];
-            v = dbl[i][v];
+    void build(int root = 0) {
+        depth.assign(n, 0);
+        parent.assign(LOG, vector<int>(n, 0));
+        dfs(root, -1, 0);
+        for (int k = 0; k + 1 < LOG; k++) {
+            for (int v = 0; v < n; v++) {
+                if (parent[k][v] < 0)
+                    parent[k + 1][v] = -1;
+                else
+                    parent[k + 1][v] = parent[k][parent[k][v]];
+            }
         }
     }
-    return dbl[0][u];
-}
+    void dfs(int cur, int par, int d) {
+        parent[0][cur] = par;
+        depth[cur] = d;
+        for (auto &nx : G[cur]) {
+            if (nx != par) dfs(nx, cur, d + 1);
+        }
+    }
+    int lca(int u, int v) {
+        assert((int)depth.size() == n);
+        if (depth[u] > depth[v]) swap(u, v);
+        // depth[u] <= depth[v](vの方が深い)
+        // 同じ深さまでvが登る
+        for (int k = 0; k < LOG; k++) {
+            if ((depth[v] - depth[u]) >> k & 1) {
+                v = parent[k][v];
+            }
+        }
+        if (u == v) return u;
+        for (int k = LOG - 1; k >= 0; k--) {
+            if (parent[k][u] != parent[k][v]) {
+                u = parent[k][u];
+                v = parent[k][v];
+            }
+        }
+        return parent[0][u];
+    }
+};
 
 int main() {
-    // 入力
-    ll n; cin >> n;
-    rep(i,n) {
-        ll k; cin >> k;
-        rep(j,k) {
-            ll c; cin >> c;
-            g[i].pb(c);
-            g[c].pb(i);
+    int n;
+    cin >> n;
+    lowest_common_ancestor Lca(n);
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        for (int j = 0; j < k; j++) {
+            int c;
+            cin >> c;
+            if (i < c) Lca.add_edge(i, c);
         }
     }
-    // 親と深さを求める
-    dfs(-1, 0);
-    // ダブリング前計算
-    rep(i,n) dbl[0][i] = parent[i];
-    rep(i,29) {
-        rep(j,n) {
-            if (dbl[i][j] == -1) dbl[i+1][j] = -1;
-            else dbl[i+1][j] = dbl[i][dbl[i][j]];
-        }
-    }
-    ll q; cin >> q;
-    while (q--) {
-        ll a, b; cin >> a >> b;
-        ll root = lca(a, b);
-        cout << root << '\n';
+    Lca.build();
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << Lca.lca(u, v) << '\n';
     }
     return 0;
 }
-
 
 /*
 // ABC014 D
@@ -106,5 +116,3 @@ int main() {
     return 0;
 }
 */
-
-
