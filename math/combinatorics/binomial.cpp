169f16373231e6fc43f6951a61e14c0831c8a859
ruthen71 <takahero2016@gmail.com>
2022-05-01 23:15:16 +0900

    change binomial

--- math/combinatorics/binomial.cpp
+++ math/combinatorics/binomial.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 // O(KlogK)
-template <class Mint>
-Mint binomial(long long N, long long K, Mint T) {
+template <class Mint> Mint binomial(long long N, long long K) {
     if (N < 0 || K < 0 || N < K) return 0;
     Mint ret1 = 1, ret2 = 1;
     K = min(K, N - K);
@@ -17,6 +16,6 @@ Mint binomial(long long N, long long K, Mint T) {
 /**
  * 使い方
  * mint ans = binomial(N, K, mint(0));
- * Tは型指定用(この使い方は良くない気がするけど解決策がわからない)
+ * Tは型指定用(この使い方は良くない気がするけど解決策がわからない)->template で解決
  * https://atcoder.jp/contests/abc205/submissions/27460765 (ABC205-E)
  */
